1e3761dcef986a77a1d9ad0dd6f71871206ba4cc
Elvio Cavalcante <elvio@madfx.com.br>
2015-02-27 15:28:08 -0300

    Solved: stringifyJSON function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,36 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+  
+	if (obj === undefined || typeof obj === 'function') {
+		return 'Not a Valid Format';
+		// range --> undefined | function.
+	}else{
+		if (obj === null || typeof obj !== 'object' && typeof obj !== 'string') {
+			// range --> boolean | number.
+			return "" + obj;
+		} else if(typeof obj === 'string'){
+			// range --> string
+			return '"' + obj + '"';
+		}else {
+			// range --> Array | Objects.
+			var result = [];
+
+			for (var key in obj) {
+				// If it's an array
+				if (typeof obj[key] !== 'function' && Array.isArray(obj) && obj[key] !== undefined) {
+					result.push(stringifyJSON(obj[key]));
+			    // If it's an object
+				} else if(typeof obj[key] !== 'function' && obj[key] !== undefined){
+					result.push('"' + key + '":' + stringifyJSON(obj[key]));
+				}
+			}
+
+			if (Array.isArray(obj)) {
+				return '[' + result + ']';
+			} else {
+				return '{' + result + '}';
+			}		
+		}
+	}
+}
